37169426885ca6f1ba8a065700c0ad94f04983a1
Alexandru-Catalin GULYASY <alexandru.gulyasy@student.upt.ro>
2026-01-11 00:23:20 +0200

    (feat) added the bb84 protocol and public chat, 3 users, Alice, Bob and Eve

--- server.js
+++ server.js
@@ -5,8 +5,59 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+let users = { Alice: null, Bob: null, Eve: null };
+
 io.on('connection', (socket) => {
     console.log('A user connected! Socket ID:', socket.id)
+
+    socket.on('join_as', (user) => {
+        if (users[user]) {
+            socket.emit('error_msg', `User ${user} already taken`);
+            return;
+        }
+
+        Object.keys(users).forEach(user => {
+            if (users[user] === socket.id) {
+                users[user] = null;
+            }
+        });
+
+        users[user] = socket.id;
+        io.emit("chat_msg", `SYSTEM:${user} has joined the chat.`);
+        socket.emit('user_assigned', user);
+    });
+
+    socket.on('alice_send', (data) => {
+
+        if (users.Eve) {
+            io.to(users.Eve).emit('eve_intercept', data);
+            io.to(users.Alice).emit('status', 'Photon sent... (Intercepted on line!)');
+        } else if (users.Bob) {
+            io.to(users.Bob).emit('bob_receive', data);
+            io.to(users.Alice).emit('status', 'Photon sent to Bob!');
+        } else {
+            socket.emit('error_msg', 'Bob is not connected yet.');
+        }
+    });
+
+    socket.on('eve_forward', (data) => {
+        if (users.Bob) {
+            io.to(users.Bob).emit('bob_receive', data);
+        }
+    });
+
+    socket.on('public_chat', (msg) => {
+        io.emit('chat_msg', msg);
+    });
+
+    socket.on('disconnect', () => {
+        Object.keys(users).forEach(user => {
+            if (users[user] === socket.id) {
+                users[user] = null;
+                io.emit("chat_msg", `SYSTEM:${user} has left the chat.`);
+            }
+        });
+    });
 });
 
 http.listen(3000, () => {
